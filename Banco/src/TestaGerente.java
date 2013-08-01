29d2224640563c1e664e28c8f0cd9a88a8ffa7d9
grmachado <glauco.r.machado@gmail.com>
2013-07-31 23:37:37 -0300

    Nova atualização

--- Banco/src/TestaGerente.java
+++ Banco/src/TestaGerente.java
@@ -2,11 +2,18 @@
 public class TestaGerente {
 
 	public static void main(String[] args) {
-		Gerente gerente = new Gerente();
-		gerente.setSalario(5000.0);
-		System.out.println(gerente.getBonificacao());
+		ControleDeBonificacoes controle = new ControleDeBonificacoes();
 		
+		Gerente funcionario1 = new Gerente();
+		funcionario1.setSalario(5000.0);
+		controle.registra(funcionario1);
+		//System.out.println(funcionario1.getBonificacao());
 		
+		Funcionario funcionario2 = new Funcionario();
+		funcionario2.setSalario(1000.0);
+		controle.registra(funcionario2);
+		
+		System.out.println(controle.getTotalDeBonificacoes());
 		//gerente.setNome("João da Silva");
 		//gerente.setSenha("4321");
 
